8414bb342017b57aeea3ed3d36987a09b567e290
Guchetan <gurchetan.singh2@icloud.com>
2026-05-17 12:01:56 +0530

    ADD: reverse a num partial solution (non negative non zero-trailing nums left)

--- basics/2_revnumber/main.cpp
+++ basics/2_revnumber/main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+int reverse(int num) {
+        int count=log10(num)+1, revnum=0;
+        while((num%10)!=0){
+            revnum+= ((num%10)* pow(10,count-1));
+	    num = num /10;
+            count--;
 
+        }
+        return revnum;
+    }
 int main(){
-	return 0;
+	int num;
+	cout<<"enter number to reverse:"<<endl;
+	cin>>num;
+	cout<<reverse(num)<<endl;
 }
 
