ca4c28b46c1a4fbf2724b9c3078193441bfc653b
Guchetan <gurchetan.singh2@icloud.com>
2026-05-18 10:49:09 +0530

    ADD: readme for problem1

---
+++ basics/1_digit/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//bruteforce approach
+int count_digits(int num){
+	int count=0;
+	while((num%10)!=0){
+		num = num /10;
+		count++;
+	}
+	
+	return count;
+}
+//optimal approach
+int count_digits_optimally(int num){
+	int count = log10(num)+1;
+	return count;
+}
+int main(){
+	int num = 2113;
+	cout<<count_digits(num)<<"count using bruteforce"<<endl;
+	num = 2113;
+	cout<<count_digits_optimally(num)<<"count using optimal approach"<<endl;
+
+
+}
