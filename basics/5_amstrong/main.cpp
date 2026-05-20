9c8121853bc808c691930a2bea2ba656a394e900
Guchetan <gurchetan.singh2@icloud.com>
2026-05-20 13:07:39 +0530

    ADD: check if amstrong problem

---
+++ basics/5_amstrong/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool chk_ams(int num){
+	int ognum = num;
+	int count = log10(num)+1;
+	int ams = 0;
+	int digit;
+	while(num!=0){
+		digit = num % 10;
+		ams += pow(digit,count);
+		num = num/10;
+	}
+	if(ams == ognum){
+		return true;
+	}
+	return false;
+}
+
+int main(){
+	int num;
+	cout<<"enter a number";
+	cin>>num;
+	if(chk_ams(num)){
+		cout<<"armstrong"<<endl;
+		return 0;
+	}
+	cout<<"not armstrong"<<endl;	
+	return 0;
+}
