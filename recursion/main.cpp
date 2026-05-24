45ed672511eaba8f5618fba64217fa519b13160a
apaleblueman <gurchetan.singh2@icloud.com>
2026-05-24 16:14:05 +0530

    ADD:fibonacci solution

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -25,11 +25,28 @@ int facto(int n){
 	if(n<1){return 1;}
 	return n * facto(n-1);
 }
+void fibonacci(int num){
+  int next=0, n1=0, n2=1;
+  if(num==0){cout<<0;return;}
+  else if(num==1){cout<<0<<" "<<1<<" ";return;}
+  else{
+  cout<<0<<" "<<1<<" ";
+  while(next<num){
+    next = n1+n2;
+    cout<<next<<" ";
+    n1=n2;
+    n2=next;
+  }}
+}
 int main(){
-	print_name(10);
+	/*print_name(10);
 	print_nums(5);
 	print_nums_rev(5);
 	cout<<endl<<sum_firstn(5)<<endl;
-	cout<<facto(5)<<endl;
-	return 0;
+	cout<<facto(5)<<endl;*/
+  int num;
+  cin>>num;
+  fibonacci(num);
+
+  return 0;
 }
