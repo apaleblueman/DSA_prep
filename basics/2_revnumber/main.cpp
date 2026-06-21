9eac9a3dc11d42c4c0812cb381208d862a721526
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-06-21 13:48:20 +0530

    ADD: reverse a number and count digits

--- basics/2_revnumber/main.cpp
+++ basics/2_revnumber/main.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
-#include<bits/stdc++.h>
 using namespace std;
     int reverse(int x) {
-        int revnum=0;
-        while(x!=0){
-                if((revnum > (INT_MAX/10)||(revnum == (INT_MAX/10)&&(x%10)>7))){ return 0; }
-                if((revnum < (INT_MIN/10)||(revnum == (INT_MIN/10)&&(x%10)<-8))){ return 0; }
-                revnum = (x % 10) + revnum*10;
-                x = x / 10;
-        }
+        int digit,revnum=0;
+	while(x > 0){
+		digit = x % 10;
+		revnum = revnum*10 + digit;
+		x = x / 10;
+	}
         return revnum;
+
     };
 int main(){
 	int num;
