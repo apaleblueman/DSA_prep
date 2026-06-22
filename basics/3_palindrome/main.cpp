250ba9781ad61714078562c5e480ccd184a45d26
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-06-22 10:54:43 +0530

    ADD: palindrome

--- basics/3_palindrome/main.cpp
+++ basics/3_palindrome/main.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 
 bool palindrome(int num){
-	int ognum = num;
-	int revnum=0;
-	while(ognum!=0){
-		revnum = (ognum%10) + (revnum*10);
-		ognum = ognum / 10;
+	int revnum,digit,ognum;
+	ognum = num;
+	while(num>0){
+		digit=num % 10;
+		revnum=(revnum*10) + digit;
+		num=num / 10;
 	}
-	cout<<revnum<<endl;
-	if(revnum == num){
+	if(revnum == ognum)
+	{
 		return true;
-	}
-	else{
+	}else{
 		return false;
 	}
-	
 }
 int main(){
 	int num;
 	cout<<"Enter a number to check if its a palindrome:";
 	cin>>num;
-	if(palindrome(num)){
-		cout<<"palindrome";
+	if(palindrome(num) == true){
+		cout<<"palindrome"<<endl;
 	}
 	else{
-		cout<<"not a palindrome";
+		cout<<"not a palindrome"<<endl;
 	}
 }
+
