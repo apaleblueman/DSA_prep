b447fec0099a7244809e93f40354202fab9128f9
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-06-22 12:06:11 +0530

    ADD: GCD approach1

--- basics/4_gcd/main.cpp
+++ basics/4_gcd/main.cpp
@@ -2,25 +2,23 @@
 using namespace std;
 
 int check_GCD(int num1, int num2){
-	int a,b,temp;
-	a = num1;
-	b = num2;
-	if(num1>num2){
-		b = num2;
-		a = num1;
+	int min=0;
+	while(num1!=0 && num2!=0){
+		if(num1>num2){
+			min = num1 - num2;
+			num1 = min;
+		}	
+		else{
+			min = num2 -num1;
+			num2 = min;
+		}
 	}
-	while(a%b!=0){
-		temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return b;
+	return num1+num2;
 }
-
 int main(){
 	int a,b;
 	cout<<"enter two numbers to check their GCD separated by spaces:";
 	cin>>a>>b;
-	cout<<check_GCD(a,b);
+	cout<<"GCD of "<<a<<","<<b<<":"<<check_GCD(a,b);
 
 }
