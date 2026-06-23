86f0f503182b73ee1d384adabe0ea26e92ade15d
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-06-23 11:17:09 +0530

    ADD: check prime

--- basics/7_primenumber/main.cpp
+++ basics/7_primenumber/main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<cmath>
+using namespace std;
 
+bool all_divisors(int num){
+        int N = sqrt(num);
+        for(int i=2;i<=N;i++){
+                if(num % i ==0){
+			return false;
+                }
+        }
+	return true;
+}
 
 int main(){
 	int num;
-	if(num
+	cin>>num;
+	if(all_divisors(num)){
+		cout<<"Prime";
+	}	
+	else{
+		cout<<"not prime";	
+	}
 	return 0;
 }
