a167fc86be8c0198bedcb31b44d526d267a7d24d
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-06-24 12:53:35 +0530

    ADD: recursion

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -1,52 +1,68 @@
 #include<iostream>
+#include<string>
+#include<vector>
+#include <algorithm>
+
 using namespace std;
-void print_name(int n){
-		if(n<=0){return;}
-		cout<<"henlo"<<n<<endl;
-		print_name(--n);
+
+void Name(string name, int N){
+	if(N<1){return;}
+	cout<<N<<name<<endl;
+	Name(name , N-1);
 }
 
-void print_nums(int N, int i=1){
-		if(i>N){return;}
-		cout<<i<<endl;
-		print_nums(N, ++i);
+void nums(int N){
+	if(N<1){return;}
+	nums(N-1);
+	cout<<N<<endl;
 }
-void print_nums_rev(int N){
-		if(N==0){return;}
-		cout<<N<<endl;
-		print_nums_rev(--N);
-}
-int sum_firstn(int N){
-	if(N<1){return N;}
-	return N + sum_firstn(N-1);//here we cant use --N because it double modifies N, and due  to recursion N is already changing via a sequence
 
+int sum(int N){
+	if(N<=0){return 0;}
+	return N + sum(N-1);
 }
-int facto(int n){
-	if(n<1){return 1;}
-	return n * facto(n-1);
+
+int facto(int N){
+	if(N<=1){return 1;}
+	return N * facto(N-1);
 }
-void fibonacci(int num){
-  int next=0, n1=0, n2=1;
-  if(num==0){cout<<0;return;}
-  else if(num==1){cout<<0<<" "<<1<<" ";return;}
-  else{
-  cout<<0<<" "<<1<<" ";
-  while(next<num){
-    next = n1+n2;
-    cout<<next<<" ";
-    n1=n2;
-    n2=next;
-  }}
+void reverseArray(vector<int>& arr){
+	int start=0, end=arr.size()-1;
+	while(start<end){
+		swap(arr[start],arr[end]);
+		start++;end--;
+	}
+}
+bool checkPalindrome(string& str){
+	int start=0, end=str.size()-1;
+	while(start<end){
+		if(str[start]==str[end]){
+			start++;end--;
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
 }
 int main(){
-	/*print_name(10);
-	print_nums(5);
-	print_nums_rev(5);
-	cout<<endl<<sum_firstn(5)<<endl;
-	cout<<facto(5)<<endl;*/
-  int num;
-  cin>>num;
-  fibonacci(num);
-
-  return 0;
+	Name("chico",10);
+	nums(10);
+	cout<<sum(6)<<endl;
+	cout<<facto(5)<<endl;
+	vector<int> arr={1,2,3,4,5};
+	reverseArray(arr);
+	for(auto i:arr){
+		cout<<i<<",";
+	}
+	cout<<endl;
+	string str = "abba a  abba";
+	if(checkPalindrome(str)){
+		cout<<"yes, palindrome";
+	}
+	else{
+		cout<<"not palindrome";
+	}
+	
+  	return 0;
 }
